fe16c478fb178efcd1322f5a94ae98485186fc90
SidAjmera <ajmera.siddhant103@gmail.com>
2025-04-23 17:30:14 +0800

    23/04/2025 5:30pm

--- colorPicker.js
+++ colorPicker.js
@@ -1,4 +1,9 @@
 var numSquares = 6;
+var rgbCode = document.getElementById('rgbCode');
+var result = document.getElementById('result');
+var resultEmoji = document.getElementById('resultEmoji');
+var button = document.getElementById('restart');
+var answered = false;
 
 function randomColor() {
      var r = Math.floor(Math.random() * 256);
@@ -17,11 +22,34 @@ function generateRandomColors(num) {
 
 function pickColor() {
      var random = Math.floor(Math.random() * numSquares);
+     return colors[random];
+     console.log(random);
 }
 var colors = generateRandomColors(numSquares);
 var square = document.querySelectorAll('.square');
-for (let i = 0; i < numSquares; i++) {
-     square[i].style.background = colors[i];
-     console.log(colors[i]);
-     console.log(square[i]);
+var pickedColor = pickColor();
+rgbCode.textContent = pickedColor;
+
+function eventHandling() {
+     for (let i = 0; i < numSquares; i++) {
+          square[i].style.background = colors[i];
+          square[i].addEventListener("click", function (){
+               var clickedColor = this.style.background;
+               if (clickedColor == pickedColor && !answered) {
+                    result.textContent = "Good Job! That's right";
+                    resultEmoji.textContent = "😱";
+                    confetti();
+                    answered = true;
+               } else if (clickedColor != pickedColor && !answered){
+                    result.textContent = "OOPS! You got it wrong!"
+                    resultEmoji.textContent = "😩"
+                    answered = true;
+               }
+          })
+     }
 }
+eventHandling();
+
+button.addEventListener("click", function(){
+     window.location.reload();
+})
