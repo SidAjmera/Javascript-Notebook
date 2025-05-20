612b246f59838cf3ff09a15c20dbc61e7a013e7f
SidAjmera <ajmera.siddhant103@gmail.com>
2025-05-20 17:35:13 +0800

    May_20_5:35_PM

--- breakout.js
+++ breakout.js
@@ -9,12 +9,12 @@ const PADDLE_WIDTH = 100;
 const PADDLE_HEIGHT = 20;
 
 const ball = {
-x: canvas.width/2,
-y: canvas.height/2, 
-radius: BALL_RADIUS, 
-speed: 4, 
-dx: -3, 
-dy: -3
+     x: canvas.width/2,
+     y: canvas.height/2, 
+     radius: BALL_RADIUS, 
+     speed: 4, 
+     dx: -3, 
+     dy: -3
 }
 
 const paddle = {
@@ -25,7 +25,26 @@ const paddle = {
      dx: 5,
 }
 
+const brick =  {
+     width: 55,
+     height: 20,
+     offsetLeft: 20,
+     fillColor: "#2e3458",
+     strokeColor: "#FFF"
+
+}
+
+canvas.addEventListener('mousemove', getMousePos);
+
+function getMousePos(evt) {
+     let rect = canvas.getBoundingClientRect();
+
+     paddle.x = evt.clientX - (PADDLE_WIDTH/2);
+}
 function update() {
+     moveBall();
+     ifOnEdgeBounce();
+     ifOnPaddleBounce();
 }
 
 function drawBall() {
@@ -49,12 +68,63 @@ function render() {
      ctx.drawImage(bg, 0, 0);
      drawBall();
      drawPaddle();
+     drawBricks();
 }
 
 function game() {
      update();
      render();
 }
+
+function moveBall() {
+     ball.x += ball.dx;
+     ball.y += ball.dy;
+}
+
+function ifOnEdgeBounce() {
+
+     // Top and bottom collision
+
+     if (ball.y < 0 || ball.y>canvas.height) {
+          ball.dy = -ball.dy;
+     }
+
+     // Left and right collision
+     if (ball.x < 0 || ball.x > canvas.width) {
+          ball.dx = -ball.dx;
+     }
+}
+
+function ifOnPaddleBounce() {
+     if (ball.y > paddle.y && ball.x < (paddle.x + paddle.width)) {
+          ball.dy = -ball.dy;
+     }
+}
+
+let bricks = [];
+
+function createBricksArray() {
+     for (let i=0; i<5; i++) {
+          bricks[i] = {
+               x: brick.offsetLeft + i * (brick.offsetLeft + brick.width),
+               y: 40,
+               status: true
+          }
+     }
+}
+
+function drawBricks() {
+     for (let i=0; i<5; i++) {
+          let b = bricks[i];
+
+          ctx.fillStyle = brick.fillColor;
+          ctx.fillRect(b.x, b.y, brick.width, brick.height);
+          ctx.strokeStyle = brick.strokeColor;
+          ctx.strokeRect(b.x, b.y, brick.width, brick.height);
+     }
+}
+
+createBricksArray();
 
 var FPS = 100;
 const myInterval = setInterval(game, 1000/FPS);
